d6a0d839f3de9573009be37fde2976ff38b336b0
Aria <nimajneb34@gmail.com>
2018-06-11 12:38:21 +0800

    add Login.js

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Connection</button>
+        <p>Don't have an account? You can create one</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data: function() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
